36e1d051eea68a0fd84cd69aefa9f7d12f8866ae
alvinskylers <alvinskylers.dev@gmail.com>
2025-11-30 23:47:14 +0800

    add: DataBaseConfig.java class -add methods to access .properties file fields -configure .gitignore file to exclude .properties file

---
+++ src/main/java/com/alvinskylers/practice/config/DatabaseConfig.java
@@ -0,0 +1,35 @@
+package com.alvinskylers.practice.config;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class DatabaseConfig {
+    private static final Properties properties = new Properties();
+
+    static{
+        try (InputStream config = DatabaseConfig.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (config != null) {
+                System.out.println("config.properties file not found...");
+                System.exit(1);
+            }
+            properties.load(config);
+
+        } catch (Exception e) {
+             e.printStackTrace();
+        }
+
+    }
+
+    static String getDatabaseURL () {
+        return properties.getProperty("config.url");
+    }
+
+    static String getUsername() {
+        return properties.getProperty("config.username");
+    }
+
+    static String getPassword() {
+        return properties.getProperty("config.password");
+    }
+
+}
